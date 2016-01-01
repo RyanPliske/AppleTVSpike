958573eb0b90943810863fadaeff2e3f353bd6a1
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-01 10:16:33 -0600

    Initial Commit

---
+++ AppleTVSpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleTVSpike
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 1/1/16.
+//  Copyright © 2016 Ryan Pliske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
