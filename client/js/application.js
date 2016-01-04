96b6e3502bffec86d38bfef91ef0221639a0ebe2
Ryan Pliske <ryan.pliske@monsanto.com>
2016-01-04 16:44:30 -0600

    Reverted Change

--- client/js/application.js
+++ client/js/application.js
@@ -4,7 +4,6 @@
     To start the server you'll run this command in my 'client' folder:
     Warning: You seriously need to be in the client directory when you run this command.
     python -m SimpleHTTPServer 9001
-
     navigationDocument is part of TVJS: https://developer.apple.com/library/tvos/documentation/TVMLJS/Reference/TVJSFrameworkReference/
     it is analogous to a UINavigationController: pushes and pops TVML documents
 */
@@ -14,7 +13,7 @@
 var resourceLoader;
 
 App.onLaunch = function(options) {
-  var javascriptFiles = ['${options.BASEURL}js/Presenter.js', '${options.BASEURL}js/ResourceLoader.js'];
+  var javascriptFiles = [`${options.BASEURL}js/Presenter.js`, `${options.BASEURL}js/ResourceLoader.js`];
   var evaluationCompletion = function(success) {
     if(success) {
         resourceLoader = new ResourceLoader(options.BASEURL);
@@ -22,15 +21,15 @@ App.onLaunch = function(options) {
             var doc = Presenter.makeDocument(resource);
             doc.addEventListener("select", Presenter.load.bind(Presenter));
             Presenter.pushDocument(doc);
-        };
-        resourceLoader.loadResource('${options.BASEURL}templates/RWDevConTemplate.xml.js', loadCompletion);
-        gameStarted('Game Was Started!');
-        console.log('Some Stuff');
+        }
+        resourceLoader.loadResource(`${options.BASEURL}templates/RWDevConTemplate.xml.js`, loadCompletion);
+        gameStarted(`Game Was Started!`);
+        console.log(`Some Stuff`);
     } else {
         var alert = createAlert("Something Went Wrong On Our Server.", "Error attempting to evaluate external JavaScript files.");
         navigationDocument.presentModal(alert);
     }
-  };
+  }
   evaluateScripts(javascriptFiles, evaluationCompletion);
 }
 
@@ -40,7 +39,7 @@ App.onLaunch = function(options) {
  */
 var createAlert = function(title, description) {
 
-    var alertString = '<?xml version="1.0" encoding="UTF-8" ?>
+    var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
         <document>
           <alertTemplate>
             <title>${title}</title>
@@ -49,7 +48,7 @@ var createAlert = function(title, description) {
                 <text>OK</text>
             </button>
           </alertTemplate>
-        </document>';
+        </document>`;
 
     // DomParser converts TVML string to TVML document (popup Window)
     var parser = new DOMParser();
